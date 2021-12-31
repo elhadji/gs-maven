cc6560409d6ad46fbfce984fe160ae48fbfc0e41
Elhadji Bougoumadiop <elhadji.bougoumadiop@loreal.com>
2021-12-31 17:39:30 +0100

    feat(EMEAMON-000): add new function

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,6 +3,9 @@ package hello;
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
+	public String sum(String a, String b){
+		return a+ b;
+	}
 	public static void main(String[] args) {
 		LocalTime currentTime = new LocalTime();
 		System.out.println("The current local time is: " + currentTime);
